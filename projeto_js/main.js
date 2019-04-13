d0652570096f234f6611525ffc87aef959b1bae6
Leorna <42484610+Leorna@users.noreply.github.com>
2019-04-12 21:02:32 -0300

    Update main.js

--- projeto_js/main.js
+++ projeto_js/main.js
@@ -82,6 +82,7 @@ const linha = function(x0, y0, x1, y1, ctx) {
 
 const desenharNumeros = function(x, y, i, ctx) {
   ctx.font = '17px Arial'
+  ctx.fillStyle = '#ffffff'
   let tmp = i
   let num = tmp / 25
   ctx.fillText(num, x, y)
